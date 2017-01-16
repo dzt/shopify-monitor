5c50290c3e3a344d8c753fe8852731262bca0915
Peter Soboyejo <thepcmrtim@gmail.com>
2017-01-16 15:22:07 -0500

    xml prep

--- tests/xml2json.js
+++ tests/xml2json.js
@@ -1,13 +1,16 @@
 const parser = require('xml2json')
 const request = require('request')
 const fs = require('fs')
+var iconv = require('iconv-lite');
+var parseString = require('xml2js').parseString
 
 request({
     url: 'https://kithnyc.com/sitemap_products_1.xml',
     method: 'get',
-    time: true,
+    encoding: null,
+    gzip: true,
     headers: {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
     }
 }, function(err, res, body) {
 
@@ -15,22 +18,21 @@ request({
         return console.log('nothing was returned')
     }
 
-    var options = {
-        object: true,
-        reversible: false,
-        coerce: false,
-        sanitize: true,
-        trim: true,
-        arrayNotation: false,
-        alternateTextNode: false
-    };
+    var bodyWithCorrectEncoding = iconv.decode(body, 'iso-8859-1');
+    //console.log(bodyWithCorrectEncoding);
 
-    var json = parser.toJson(body, options)
+    parseString(bodyWithCorrectEncoding, function(err, result) {
+        console.dir(result);
+        console.log(result.urlset.url)
+        // console.log(result)
+    });
 
+    /*
     fs.writeFile('test.json', json, (err) => {
         if (err) throw err;
         console.log('It\'s saved!');
     });
+    */
 
 
 });
